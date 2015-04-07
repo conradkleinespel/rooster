ca9819ea05de3c5acbeb475740bb5756d137adc3
Conrad Kleinespel <conradk@conradk.com>
2015-04-07 10:27:44 +0200

    update serialize

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@
 
 extern crate libc;
 extern crate getopts;
-extern crate "rustc-serialize" as rustc_serialize;
+extern crate rustc_serialize;
 extern crate crypto;
 extern crate rpassword;
 extern crate rand;

ce8f5cd32a114fe29778319e75fab61120aac667
Conrad Kleinespel <conradk@conradk.com>
2017-01-04 18:58:10 +0100

    improves output of "rooster list" when piped to grep

--- src/commands/list.rs
+++ src/commands/list.rs
@@ -15,6 +15,7 @@
 use super::super::getopts;
 use super::super::password;
 use std::iter::{Iterator, FromIterator, repeat};
+use libc::isatty;
 
 pub fn callback_help() {
     println!("Usage:");
@@ -30,20 +31,28 @@ pub fn callback_exec(_matches: &getopts::Matches,
                      -> Result<(), i32> {
     let all_passwords = store.get_all_passwords();
 
+    let output_is_piped = unsafe { isatty(1) } == 0;
+
     if all_passwords.len() == 0 {
-        println!("No passwords on record yet. Add one with 'rooster add <app> <username>'.");
+        if !output_is_piped {
+            println!("No passwords on record yet. Add one with 'rooster add <app> <username>'.");
+        }
     } else {
         // We'll now print the password in a table.
         // The table is delimited by borders.
         let horizontal_border = String::from_iter(repeat('-').take(73));
 
-        println!("{}", horizontal_border);
-        println!("| {:2} | {:30} | {:30} |", "id", "app", "username");
-        println!("{}", horizontal_border);
+        if !output_is_piped {
+            println!("{}", horizontal_border);
+            println!("| {:2} | {:30} | {:30} |", "id", "app", "username");
+            println!("{}", horizontal_border);
+        }
         for (i, p) in all_passwords.iter().enumerate() {
             println!("| {:2?} | {:30} | {:30} |", i, p.name, p.username);
         }
-        println!("{}", horizontal_border);
+        if !output_is_piped {
+            println!("{}", horizontal_border);
+        }
     }
 
     Ok(())

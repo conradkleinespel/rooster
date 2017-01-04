c2c1425ac2c771bfa003dffd3f0234aff3c70bb0
Conrad Kleinespel <conradk@conradk.com>
2017-01-04 19:20:57 +0100

    updates copyright notice

--- src/commands/change.rs
+++ src/commands/change.rs
@@ -1,4 +1,4 @@
-// Copyright 2014 The Rooster Developers
+// Copyright 2014-2017 The Rooster Developers
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.

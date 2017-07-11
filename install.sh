9dd7f0789bf90755885797f09393b8b36d5b2480
varin-pierre <pvarin@student.42.fr>
2017-07-11 15:00:50 +0200

    fix script for Macintosh and still work on Ubuntu

--- install.sh
+++ install.sh
@@ -3,14 +3,24 @@
 pkgname=rooster
 pkgver=2.6.0
 sha256=ad453e7f937b8482c94283ce26d4982386aba956b8f914f9e7b55c760378ef1f
+MacOS=false
 
-# Arch Linux gets its own package on the AUR
-cat /etc/*-release | grep -i 'Arch Linux' > /dev/null
-if [ "$?" = "0" ]; then
-    echo 'Looks like you are using Arch Linux. You can find Rooster on the AUR:'
-    echo 'https://aur.archlinux.org/packages/rooster'
-    exit
+
+# uname for check if MacOS
+if [ "$(uname)" = "Darwin" ];then
+	echo "Looks like you are using an Macintosh."
+	$MacOS = true
+else
+	# Arch Linux gets its own package on the AUR
+	cat /etc/*-release | grep -i 'Arch Linux' > /dev/null
+	if [ "$?" = "0" ]; then
+	    echo 'Looks like you are using Arch Linux. You can find Rooster on the AUR:'
+	    echo 'https://aur.archlinux.org/packages/rooster'
+	    exit
+	fi
 fi
+
+
 
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 if [ "$?" != "0" ]; then
@@ -23,52 +33,54 @@ else
     export PATH="$HOME/.cargo/bin:$PATH"
 fi
 
-# ubuntu/debian
-distro="`lsb_release -is`"
-if [ "$?" = "0" ]; then
-    if [ "$distro" = "Ubuntu" ]; then
-        version="`lsb_release -rs`"
-        if [ "$?" = "0" ]; then
-            if [ "$version" = "16.04" -o "$version" = "16.10" ]; then
-                sudo apt update -y && sudo apt install -y unzip pkg-config libx11-dev libxmu-dev
-                if [ "$?" != "0" ]; then
-                    echo 'aborting: could not install rooster dependencies' 1>&2
-                    exit 1
-                fi
-            elif [ "$version" = "14.04" ]; then
-                sudo apt-get update -y && sudo apt-get install -y unzip pkg-config libx11-dev libxmu-dev
-                if [ "$?" != "0" ]; then
-                    echo 'aborting: could not install rooster dependencies' 1>&2
-                    exit 1
-                fi
-            fi
-        fi
-    elif [ "$distro" = "Debian" ]; then
-        sudo apt-get install -y gcc unzip pkg-config libx11-dev libxmu-dev
-        if [ "$?" != "0" ]; then
-            echo 'aborting: could not install rooster dependencies' 1>&2
-            exit 1
-        fi
-    fi
-fi
+MacOS ];then
+	# ubuntu/debian
+	distro="`lsb_release -is`"
+	if [ "$?" = "0" ]; then
+	    if [ "$distro" = "Ubuntu" ]; then
+	        version="`lsb_release -rs`"
+	        if [ "$?" = "0" ]; then
+	            if [ "$version" = "16.04" -o "$version" = "16.10" ]; then
+	                sudo apt update -y && sudo apt install -y unzip pkg-config libx11-dev libxmu-dev
+	                if [ "$?" != "0" ]; then
+	                    echo 'aborting: could not install rooster dependencies' 1>&2
+	                    exit 1
+	                fi
+	            elif [ "$version" = "14.04" ]; then
+	                sudo apt-get update -y && sudo apt-get install -y unzip pkg-config libx11-dev libxmu-dev
+	                if [ "$?" != "0" ]; then
+	                    echo 'aborting: could not install rooster dependencies' 1>&2
+	                    exit 1
+	                fi
+	            fi
+	        fi
+	    elif [ "$distro" = "Debian" ]; then
+	        sudo apt-get install -y gcc unzip pkg-config libx11-dev libxmu-dev
+	        if [ "$?" != "0" ]; then
+	            echo 'aborting: could not install rooster dependencies' 1>&2
+	            exit 1
+	        fi
+	    fi
+	fi
 
-# fedora/centos with dnf/yum
-dnf -h > /dev/null
-dnfstatus="$?"
-yum -h > /dev/null
-yumstatus="$?"
-if [ "$dnfstatus" = "0" ]; then
-    sudo dnf install -y gcc unzip pkgconfig libX11-devel libXmu-devel
-    if [ "$?" != "0" ]; then
-        echo 'aborting: could not install rooster dependencies' 1>&2
-        exit 1
-    fi
-elif [ "$yumstatus" = "0" ]; then
-    sudo yum install -y gcc unzip pkgconfig libX11-devel libXmu-devel
-    if [ "$?" != "0" ]; then
-        echo 'aborting: could not install rooster dependencies' 1>&2
-        exit 1
-    fi
+	# fedora/centos with dnf/yum
+	dnf -h > /dev/null
+	dnfstatus="$?"
+	yum -h > /dev/null
+	yumstatus="$?"
+	if [ "$dnfstatus" = "0" ]; then
+	    sudo dnf install -y gcc unzip pkgconfig libX11-devel libXmu-devel
+	    if [ "$?" != "0" ]; then
+	        echo 'aborting: could not install rooster dependencies' 1>&2
+	        exit 1
+	    fi
+	elif [ "$yumstatus" = "0" ]; then
+	    sudo yum install -y gcc unzip pkgconfig libX11-devel libXmu-devel
+	    if [ "$?" != "0" ]; then
+	        echo 'aborting: could not install rooster dependencies' 1>&2
+	        exit 1
+	    fi
+	fi
 fi
 
 rm -rf /tmp/$pkgname-$pkgver /tmp/$pkgname-$pkgver.tar.gz
@@ -79,7 +91,12 @@ if [ "$?" != "0" ]; then
     exit 1
 fi
 
-actual_sha256="`sha256sum /tmp/$pkgname-$pkgver.tar.gz | cut -d' ' -f1`"
+if [ MacOS ]; then
+	# cmd shasum256 doesn't work on MacOS replace by shasum -a 256
+	actual_sha256="`shasum -a 256 /tmp/$pkgname-$pkgver.tar.gz | cut -d' ' -f1`"
+else
+	actual_sha256="`sha256sum /tmp/$pkgname-$pkgver.tar.gz | cut -d' ' -f1`"
+fi
 if [ "$actual_sha256" != "$sha256" ]; then
     echo 'aborting: could not verify file signature' 1>&2
     exit 1
@@ -101,12 +118,23 @@ if [ "$buildstatus" != "0" ]; then
 fi
 
 # copy binaries to /usr/bin
-sudo cp /tmp/$pkgname-$pkgver/target/release/rooster-clipboard /usr/bin/rooster-clipboard
+
+if [ MacOS ]; then
+	# File must be copy in /usr/local
+	sudo cp /tmp/$pkgname-$pkgver/target/release/rooster-clipboard /usr/local/bin/rooster-clipboard
+else
+	sudo cp /tmp/$pkgname-$pkgver/target/release/rooster-clipboard /usr/bin/rooster-clipboard
+fi
 if [ "$?" != "0" ]; then
     echo 'aborting: could not copy rooster-clipboard' 1>&2
     exit 1
 fi
-sudo cp /tmp/$pkgname-$pkgver/target/release/rooster /usr/bin/rooster
+
+if [ MacOS ]; then
+	sudo cp /tmp/$pkgname-$pkgver/target/release/rooster /usr/local/bin/rooster
+else
+	sudo cp /tmp/$pkgname-$pkgver/target/release/rooster /usr/bin/rooster
+fi
 if [ "$?" != "0" ]; then
     echo 'aborting: could not copy rooster' 1>&2
     exit 1

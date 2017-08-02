5db3f7acfdbd5cca30725125eaec432aa40f10ad
varin-pierre <pvarin@student.42.fr>
2017-08-02 12:45:24 +0200

    check norme intall.sh

--- install.sh
+++ install.sh
@@ -7,104 +7,101 @@ MacOS=false
 
 # uname for check if MacOS
 if [ "$(uname)" = "Darwin" ];then
-	echo "Looks like you are using an Macintosh."
-	$MacOS = true
+    echo "Looks like you are using an Macintosh."
+    $MacOS = true
 else
-	# Arch Linux gets its own package on the AUR
-	cat /etc/*-release | grep -i 'Arch Linux' > /dev/null
-	if [ "$?" = "0" ]; then
-		echo 'Looks like you are using Arch Linux. You can find Rooster on the AUR:'
-		echo 'https://aur.archlinux.org/packages/rooster'
-		exit
-	fi
+    # Arch Linux gets its own package on the AUR
+    cat /etc/*-release | grep -i 'Arch Linux' > /dev/null
+    if [ "$?" = "0" ]; then
+        echo 'Looks like you are using Arch Linux. You can find Rooster on the AUR:'
+        echo 'https://aur.archlinux.org/packages/rooster'
+        exit
+    fi
 fi
-
-
 
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 if [ "$?" != "0" ]; then
-	echo 'aborting: could not install rust' 1>&2
-	exit 1
+    echo 'aborting: could not install rust' 1>&2
+    exit 1
 fi
 if [ "$CARGO_HOME" != "" ]; then
-	export PATH="$CARGO_HOME/bin:$PATH"
+    export PATH="$CARGO_HOME/bin:$PATH"
 else
-	export PATH="$HOME/.cargo/bin:$PATH"
+    export PATH="$HOME/.cargo/bin:$PATH"
 fi
 
-if [ ! MacOS ];then
-	# ubuntu/debian
-	distro="`lsb_release -is`"
-	if [ "$?" = "0" ]; then
-		if [ "$distro" = "Ubuntu" ]; then
-			version="`lsb_release -rs`"
-			if [ "$?" = "0" ]; then
-				if [ "$version" = "16.04" -o "$version" = "16.10" ]; then
-					sudo apt update -y && sudo apt install -y unzip pkg-config libx11-dev libxmu-dev
-					if [ "$?" != "0" ]; then
-						echo 'aborting: could not install rooster dependencies' 1>&2
-						exit 1
-					fi
-				elif [ "$version" = "14.04" ]; then
-					sudo apt-get update -y && sudo apt-get install -y unzip pkg-config libx11-dev libxmu-dev
-					if [ "$?" != "0" ]; then
-						echo 'aborting: could not install rooster dependencies' 1>&2
-						exit 1
-					fi
-				fi
-			fi
-		elif [ "$distro" = "Debian" ]; then
-			sudo apt-get install -y gcc unzip pkg-config libx11-dev libxmu-dev
-			if [ "$?" != "0" ]; then
-				echo 'aborting: could not install rooster dependencies' 1>&2
-				exit 1
-			fi
-		fi
-	fi
-
-	# fedora/centos with dnf/yum
-	dnf -h > /dev/null
-	dnfstatus="$?"
-	yum -h > /dev/null
-	yumstatus="$?"
-	if [ "$dnfstatus" = "0" ]; then
-		sudo dnf install -y gcc unzip pkgconfig libX11-devel libXmu-devel
-		if [ "$?" != "0" ]; then
-			echo 'aborting: could not install rooster dependencies' 1>&2
-			exit 1
-		fi
-	elif [ "$yumstatus" = "0" ]; then
-		sudo yum install -y gcc unzip pkgconfig libX11-devel libXmu-devel
-		if [ "$?" != "0" ]; then
-			echo 'aborting: could not install rooster dependencies' 1>&2
-			exit 1
-		fi
-	fi
+# ubuntu/debian
+distro="`lsb_release -is`"
+if [ "$?" = "0" ]; then
+    if [ "$distro" = "Ubuntu" ]; then
+        version="`lsb_release -rs`"
+        if [ "$?" = "0" ]; then
+            if [ "$version" = "16.04" -o "$version" = "16.10" ]; then
+                sudo apt update -y && sudo apt install -y unzip pkg-config libx11-dev libxmu-dev
+                if [ "$?" != "0" ]; then
+                    echo 'aborting: could not install rooster dependencies' 1>&2
+                    exit 1
+                fi
+            elif [ "$version" = "14.04" ]; then
+                sudo apt-get update -y && sudo apt-get install -y unzip pkg-config libx11-dev libxmu-dev
+                if [ "$?" != "0" ]; then
+                    echo 'aborting: could not install rooster dependencies' 1>&2
+                    exit 1
+                fi
+            fi
+        fi
+    elif [ "$distro" = "Debian" ]; then
+        sudo apt-get install -y gcc unzip pkg-config libx11-dev libxmu-dev
+        if [ "$?" != "0" ]; then
+            echo 'aborting: could not install rooster dependencies' 1>&2
+            exit 1
+        fi
+    fi
 fi
+
+# fedora/centos with dnf/yum
+dnf -h > /dev/null
+dnfstatus="$?"
+yum -h > /dev/null
+yumstatus="$?"
+if [ "$dnfstatus" = "0" ]; then
+    sudo dnf install -y gcc unzip pkgconfig libX11-devel libXmu-devel
+    if [ "$?" != "0" ]; then
+        echo 'aborting: could not install rooster dependencies' 1>&2
+        exit 1
+    fi
+elif [ "$yumstatus" = "0" ]; then
+    sudo yum install -y gcc unzip pkgconfig libX11-devel libXmu-devel
+    if [ "$?" != "0" ]; then
+        echo 'aborting: could not install rooster dependencies' 1>&2
+        exit 1
+    fi
+fi
+
 
 rm -rf /tmp/$pkgname-$pkgver /tmp/$pkgname-$pkgver.tar.gz
 
 curl -sSL https://crates.io/api/v1/crates/$pkgname/$pkgver/download -o /tmp/$pkgname-$pkgver.tar.gz
 if [ "$?" != "0" ]; then
-	echo 'aborting: could not download rooster' 1>&2
-	exit 1
+    echo 'aborting: could not download rooster' 1>&2
+    exit 1
 fi
 
 if [ MacOS ]; then
-	# cmd shasum256 doesn't work on MacOS replace by shasum -a 256
-	actual_sha256="`shasum -a 256 /tmp/$pkgname-$pkgver.tar.gz | cut -d' ' -f1`"
+    # cmd shasum256 doesn't work on MacOS replace by shasum -a 256
+    actual_sha256="`shasum -a 256 /tmp/$pkgname-$pkgver.tar.gz | cut -d' ' -f1`"
 else
-	actual_sha256="`sha256sum /tmp/$pkgname-$pkgver.tar.gz | cut -d' ' -f1`"
+    actual_sha256="`sha256sum /tmp/$pkgname-$pkgver.tar.gz | cut -d' ' -f1`"
 fi
 if [ "$actual_sha256" != "$sha256" ]; then
-	echo 'aborting: could not verify file signature' 1>&2
-	exit 1
+    echo 'aborting: could not verify file signature' 1>&2
+    exit 1
 fi
 
 tar -C /tmp -zxvf /tmp/$pkgname-$pkgver.tar.gz
 if [ "$?" != "0" ]; then
-	echo 'aborting: could not untar rooster' 1>&2
-	exit 1
+    echo 'aborting: could not untar rooster' 1>&2
+    exit 1
 fi
 
 cd /tmp/$pkgname-$pkgver
@@ -112,29 +109,29 @@ cargo build --release
 buildstatus="$?"
 cd -
 if [ "$buildstatus" != "0" ]; then
-	echo 'aborting: could not build rooster' 1>&2
-	exit 1
+    echo 'aborting: could not build rooster' 1>&2
+    exit 1
 fi
 
 # copy binaries to /usr/bin
 
 if [ MacOS ]; then
-	# File must be copy in /usr/local
-	sudo cp /tmp/$pkgname-$pkgver/target/release/rooster-clipboard /usr/local/bin/rooster-clipboard
+    # File must be copy in /usr/local
+    sudo cp /tmp/$pkgname-$pkgver/target/release/rooster-clipboard /usr/local/bin/rooster-clipboard
 else
-	sudo cp /tmp/$pkgname-$pkgver/target/release/rooster-clipboard /usr/bin/rooster-clipboard
+    sudo cp /tmp/$pkgname-$pkgver/target/release/rooster-clipboard /usr/bin/rooster-clipboard
 fi
 if [ "$?" != "0" ]; then
-	echo 'aborting: could not copy rooster-clipboard' 1>&2
-	exit 1
+    echo 'aborting: could not copy rooster-clipboard' 1>&2
+    exit 1
 fi
 
 if [ MacOS ]; then
-	sudo cp /tmp/$pkgname-$pkgver/target/release/rooster /usr/local/bin/rooster
+    sudo cp /tmp/$pkgname-$pkgver/target/release/rooster /usr/local/bin/rooster
 else
-	sudo cp /tmp/$pkgname-$pkgver/target/release/rooster /usr/bin/rooster
+    sudo cp /tmp/$pkgname-$pkgver/target/release/rooster /usr/bin/rooster
 fi
 if [ "$?" != "0" ]; then
-	echo 'aborting: could not copy rooster' 1>&2
-	exit 1
+    echo 'aborting: could not copy rooster' 1>&2
+    exit 1
 fi

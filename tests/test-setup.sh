0b37881ccfeaf6a2d7e4450da126045323b7c657
Conrad Kleinespel <conradk@conradk.com>
2017-09-01 15:03:59 +0000

    30DaysOfTests: rooster file created on first run

---
+++ tests/test-setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# create a new empty volume for tests
+docker volume rm rooster >& /dev/null
+docker volume create rooster >& /dev/null
+
+# don't create the file
+printf 'y\nxxxx\n' | docker run --rm -i -v rooster:/home/rooster rooster list || exit 1 
+
+# test that the file is there
+docker run --rm -v rooster:/data --entrypoint /bin/sh \
+    busybox -c 'test "`ls -a1 /data | grep .passwords.rooster | wc -l`" = 1' || exit 2
+
+# test that file need not be recreated on a new run
+printf 'xxxx\n' | docker run --rm -i -v rooster:/home/rooster rooster list || exit 3
+

327dac0c45d165e93f3780f1257116d47c16fd06
Chad Baxter <cbaxter@mail.umw.edu>
2018-02-13 18:23:18 -0800

    Update uninstall.rs

--- src/commands/uninstall.rs
+++ src/commands/uninstall.rs
@@ -53,7 +53,7 @@ pub fn callback_exec(_matches: &getopts::Matches) -> Result<(), i32> {
 
     if let Some((filename, from_env)) = get_password_file_path().ok() {
         println!();
-        println!("If you want to remove your password file as well, you can - just make sure you don't lock yourself out of your online accounts. It is located at:");
+        println!("If you want to remove your password file as well, you can - just make sure you do not lock yourself out of your online accounts. It is located at:");
         println!("    {}", filename.to_string_lossy().deref());
         if from_env {
             println!();

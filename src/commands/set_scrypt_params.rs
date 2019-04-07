bd373fcf85691efa314457de2b7c2fa00841b6a9
Conrad Kleinespel <conradk@conradk.com>
2019-04-07 19:07:29 +0200

    adds set-scrypt-params command

---
+++ src/commands/set_scrypt_params.rs
@@ -0,0 +1,56 @@
+// Copyright 2014-2017 The Rooster Developers
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use getopts;
+use password;
+use std::io::Write;
+
+pub fn callback_help() {
+    println!("Usage:");
+    println!("    rooster set-scrypt-params -h");
+    println!("    rooster set-scrypt-params <log2n> <r> <p>");
+    println!();
+    println!("Example:");
+    println!("    rooster set-scrypt-params 12 8 1");
+}
+
+pub fn check_args(matches: &getopts::Matches) -> Result<(), i32> {
+    if matches.free.len() < 4 {
+        println_err!("Woops, seems like a param is missing here. For help, try:");
+        println_err!("    rooster set-scrypt-params -h");
+        return Err(1);
+    }
+
+    Ok(())
+}
+
+pub fn callback_exec(
+    matches: &getopts::Matches,
+    store: &mut password::v2::PasswordStore,
+) -> Result<(), i32> {
+    check_args(matches)?;
+
+    let log2_n = matches.free[1].trim().parse::<u8>().unwrap();
+    let r = matches.free[2].trim().parse::<u32>().unwrap();
+    let p = matches.free[3].trim().parse::<u32>().unwrap();
+
+    if log2_n > 20 || r > 8 || p > 1 {
+        println_err!("These parameters seem very high. You might be unable to open your password file ever again. Aborting.");
+        return Err(1)
+    }
+
+    store.change_scrypt_params(log2_n, r, p);
+
+    Ok(())
+}

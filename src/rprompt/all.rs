58dfaef033616e911d00e8b750f6829239c3c84f
Johnathan Sharratt <johnathan.sharratt@gmail.com>
2021-10-16 09:13:43 +0200

    Added the start of support for WASM/WASI

--- src/rprompt/all.rs
+++ src/rprompt/all.rs
@@ -1,28 +1,3 @@
-//! This library makes it easy to prompt for input in a console application on all platforms, Unix
-//! and Windows alike.
-//!
-//! Here's how you can prompt for a reply:
-//! ```no_run
-//! let name = rprompt::prompt_reply("What's your name? ").unwrap();
-//! println!("Your name is {}", name);
-//! ```
-//!
-//! Alternatively, you can read the reply without prompting:
-//! ```no_run
-//! let name = rprompt::read_reply().unwrap();
-//! println!("Your name is {}", name);
-//! ```
-//!
-//! If you need more control over the source of the input, which can be useful if you want to unit
-//! test your CLI or handle pipes gracefully, you can use `from_bufread` versions of the functions
-//! and pass any reader you want:
-//! ```no_run
-//! let stdin = std::io::stdin();
-//! let stdout = std::io::stdout();
-//! let name = rprompt::prompt_reply_from_bufread(&mut stdin.lock(), &mut stdout.lock(), "What's your name? ").unwrap();
-//! println!("Your name is {}", name);
-//! ```
-
 use crate::rutil::fix_new_line::fix_new_line;
 use crate::rutil::print_tty::{print_tty, print_writer};
 use std::io::{BufRead, BufReader, Write};
